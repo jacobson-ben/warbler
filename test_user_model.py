ec2e30c6f722fb035b36c7acfeeb99741485e3d0
johnathanraiss <johnathan.raiss@gmail.com>
2021-03-18 13:16:09 -0400

    repr test written, is_following tests causing terminal to freeze

--- test_user_model.py
+++ test_user_model.py
@@ -7,8 +7,8 @@
 
 import os
 from unittest import TestCase
-
 from models import db, User, Message, Follows
+from sqlalchemy import exc
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -33,6 +33,16 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
+
+        u1 = User.signup("bob", "bbb@hotmail.com", "12345", None)
+        u1.id = 1111
+
+        u2 = User.signup("coco", "coco@hotmail.com", "23456", None)
+        u2.id = 2222
+        db.session.add(u1, u2)
+        db.session.commit()
 
         User.query.delete()
         Message.query.delete()
@@ -55,3 +65,27 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_repr(self):
+        """Testing correct output representation for repr method"""
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+        # return repr(User(username="John")) == "<User: {'username': 'John'}>"
+        return repr(User(username="testuser")) == "<User: {'username': 'testuser'}>"
+
+    def test_is_following(self):
+        """Detecting if user follows another user"""
+
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        self.assestTrue(self.u2.is_followed_by(self.u1))
+        self.assertFalse(self.u1.is_followed_by(self.u2))
+
