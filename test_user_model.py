2ee6c0a0334acd47e5633bfd4a999516bb8840cf
Ben Jacobson <jacobson.ben11@gmail.com>
2021-03-18 14:33:57 -0400

    testing followers/following and signup

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy import exc
 
 from models import db, User, Message, Follows
 
@@ -40,6 +41,26 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+        u1 = User(username="Benny", email="testing11@gmail.com", password="testing")
+        u1.id = 1000
+
+        u2 = User(username="Zacky", email="testing12@gmail.com", password="testing")
+        u2.id = 2000
+
+        u3 = User(username="Johnathan", email="testing13@gmail.com", password="testing")
+        u3.id = 3000
+
+        db.session.commit()
+
+        self.u1 = u1
+        self.u2 = u2
+        self.u3 = u3
+
+
+    def tearDown(self):
+        db.session.rollback()
+
+
     def test_user_model(self):
         """Does basic model work?"""
 
@@ -55,3 +76,70 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    
+    def test_repr(self):
+        """Does the repr work?"""
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        self.assertEqual(repr(u), f"<User #{u.id}: testuser, test@test.com>")
+    
+    
+    def test_following(self):
+
+        self.u1.following.append(self.u2)
+        db.session.commit()
+        self.assertIn(self.u2, self.u1.following)
+        self.assertEqual(len(self.u1.following), 1)
+        
+        self.u1.following.append(self.u3)
+        db.session.commit()
+        self.assertEqual(len(self.u1.following), 2)
+
+        self.u1.following.remove(self.u2)
+        db.session.commit()
+        self.assertNotIn(self.u2, self.u1.following)
+        self.assertEqual(len(self.u1.following), 1)
+
+    
+    def test_followers(self):
+
+        self.u1.followers.append(self.u2)
+        db.session.commit()
+        self.assertIn(self.u2, self.u1.followers)
+        self.assertEqual(len(self.u1.followers), 1)
+
+        self.u1.followers.remove(self.u2)
+        db.session.commit()
+        self.assertNotIn(self.u2, self.u1.followers)
+        self.assertEqual(len(self.u1.followers), 0)
+
+    
+    def test_signup(self):
+        
+        test_user = User.signup("Teddy", "testing20@gmail.com", "testing", "")
+        test_user.id = 4000
+        db.session.add(test_user)
+        db.session.commit()
+
+        user = User.query.get_or_404(test_user.id)
+
+        self.assertEqual(user.id, 4000)
+        self.assertEqual(user.username, "Teddy")
+
+    
+    def test_signup_fail(self):
+        
+        test_user = User.signup("T", None, "testing", None)
+        with self.assertRaises(exc.IntegrityError): 
+            db.session.commit()
+        
+
